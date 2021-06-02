1af413fb7c8a43837803daeb3289fd3866826768
PXLcat <pixellecat@gmail.com>
2021-06-02 16:43:39 +0200

    first commit

---
+++ src/main/java/fr/eni/TPfilmotheque/TPfilmothequeApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.TPfilmotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TPfilmothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TPfilmothequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/TPfilmotheque/TPfilmothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.TPfilmotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TPfilmothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
